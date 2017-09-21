ebf244d7f296e18082195840ba1c451ec9811a01
Chaitanya Saladi <chaitanya.snv@gmail.com>
2017-09-21 15:17:39 -0500

    Add files via upload

---
+++ SymmetricNumbers.js
@@ -0,0 +1,32 @@
+function calculateSymmetricNumbers(event) {
+    const currentLength = input.length;
+    let current = String(Number(input) + 1);
+    let isSymmetric = false;
+    while (isSymmetric === false) {
+        // Logic to find symmetry when number of digits is even
+        if (currentLength > 2 && currentLength % 2 === 0) {
+            let firstHalf = String(current).substring(0, currentLength / 2);
+            let secondHalf = String(current).substring(currentLength / 2, currentLength);
+            secondHalf = secondHalf.split('').reverse().join('');
+            if (firstHalf === secondHalf) {
+                isSymmetric = true;
+                console.log("Symmetrical number", current);
+                return;
+            } else {
+                current++;
+            }
+            // When odd number of digits
+        } else {
+            let centerChar = String(current).charAt(currentLength / 2);
+            let firstHalf = String(current).substring(0, currentLength / 2);
+            let secondHalf = String(current).substring(currentLength / 2 + 1, currentLength);
+            if (firstHalf === secondHalf) {
+                isSymmetric = true;
+                console.log("Symmetrical number", current);
+                return;
+            } else {
+                current++;
+            }
+        }
+    }
+}
